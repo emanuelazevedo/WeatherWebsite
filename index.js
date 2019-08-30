e4456d5efd1b3ed64e7d42a55574de465e7c794e
Emanuel <emanuel.azevedo@ua.pt>
2019-08-30 18:35:28 +0100

    body parser para tentar obter os dados do body

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
+const bodyParser = require('body-parser')
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
@@ -17,14 +18,24 @@ const server = http.createServer((req, res) => {
 
     if (req.url === '/api/weather') {
         axios.get('https://api.weatherbit.io/v2.0/current?city=Aveiro&key=6accba4c460e4434ba36f3d1a5ac1031')
-            .then(res => {
-                const data = res.data.data;
-                console.log(data[0].city_name);
-                // console.log(res.data.explanation);
+            .then(response => {
+                const data = response.data.data;
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                // res.end(JSON.stringify(data[0].city_name));
+                res.end(JSON.stringify(req.body));
+
             })
             .catch(error => {
                 console.log("Error:" + error);
             });
+        // let body = '';
+        // req.on('data', chunk => {
+        //     body += chunk.toString(); // convert Buffer to string
+        // });
+        // req.on('end', () => {
+        //     console.log(parse(body));
+        //     res.end('ok');
+        // });
     }
 });
 
