c5e7d8a0ec6b08f2be47f6f1d8e48515d31a13a6
Emanuel <emanuel.azevedo@ua.pt>
2019-08-30 18:57:08 +0100

    adiçao do logs, ainda por ter as cidades

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
-const bodyParser = require('body-parser')
+const os = require('os');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
@@ -17,17 +17,34 @@ const server = http.createServer((req, res) => {
     }
 
     if (req.url === '/api/weather') {
+
+        var date = new Date();
+
+        //Chamada a API
         axios.get('https://api.weatherbit.io/v2.0/current?city=Aveiro&key=6accba4c460e4434ba36f3d1a5ac1031')
             .then(response => {
                 const data = response.data.data;
                 res.writeHead(200, { 'Content-Type': 'application/json' });
                 // res.end(JSON.stringify(data[0].city_name));
-                res.end(JSON.stringify(req.body));
+                res.end(JSON.stringify(data[0].city_name));
 
             })
             .catch(error => {
                 console.log("Error:" + error);
             });
+
+
+        //Ficheiro de logs(momento, plataforma e cidades)
+        fs.appendFile('logs.txt', 
+        date + " - " + os.platform + " Searched for:" + "\n", 
+        (err) => {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file!');
+        });
+
+
+
+
         // let body = '';
         // req.on('data', chunk => {
         //     body += chunk.toString(); // convert Buffer to string
