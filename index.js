57bed9ba7cd2fe50f99dff4d26b685a881b755d5
Emanuel <emanuel.azevedo@ua.pt>
2019-09-03 11:01:03 +0100

    alteraçao ao loop para retornar apenas quando o array estiver percorrido

--- index.js
+++ index.js
@@ -6,22 +6,41 @@ const os = require('os');
 const request = require('request');
 
 //Chamada a API request
-const apiCallRequest = (callback, city) => {
-    request('https://api.weatherbit.io/v2.0/current?city='+city+'&key=6accba4c460e4434ba36f3d1a5ac1031', { json: true }, (err, res, body) => {
-        if (err) {
-            return callback(err);
-        }
-        const data = body.data;
-        console.log('callback', callback)
-        dataResult = {
-            city: data[0].city_name,
-            sunrise: data[0].sunrise,
-            sunset: data[0].sunset,
-            temp: data[0].temp,
-        }
-        console.log('dataResultCallback', dataResult)
-        return callback(dataResult);
-    });
+const apiCallRequest = (callback, cities) => {
+    
+    console.log('callback', cities)
+    let teste = [];
+    let size = cities.length;
+    cities.forEach((city, index) => {
+        
+        console.log('city', city)
+        console.log('index', index)
+        request('https://api.weatherbit.io/v2.0/current?city='+city+'&key=6accba4c460e4434ba36f3d1a5ac1031', { json: true }, (err, res, body) => {
+            if (err) {
+                return callback("Erro na chamada "+err);
+            }
+            const data = body.data;
+
+            dataResult = {
+                city: data[0].city_name,
+                sunrise: data[0].sunrise,
+                sunset: data[0].sunset,
+                temp: data[0].temp,
+            }
+            console.log('dataResultCallback', dataResult)
+            // return callback(dataResult);
+            teste.push(dataResult);
+            console.log('teste', teste)
+
+            //verifica se o array foi todo percorrido
+            if((index+1)==size){
+                console.log('return', teste)
+                return callback(teste);
+            }
+            
+        });
+    }); 
+    
 }
 
 const server = http.createServer((req, res) => {
@@ -48,69 +67,36 @@ const server = http.createServer((req, res) => {
         req.on('end', function () {
 
             postBody = JSON.parse(body);
-            console.log('postBody', postBody.city1)
-            var city1 = postBody.city1;
+            console.log('postBody', postBody)
+            var cities = postBody.city;
+            //Chamada a API enviando um array com as cidades que pretendo pesquisar
             apiCallRequest(function (response) {
-                //console.log(JSON.stringify(response));
+                console.log('response', response)
                 res.write(JSON.stringify(response));
                 res.end();
-            }, city1);
+            }, cities);
             
         });
         
     
+        //Ficheiro de logs(momento, plataforma e cidades)
+        function createLog(data){
+            let cities = " ";
+            let temperatures = " ";
+            // console.log('data', data)
+            data.forEach(test => {
+                // console.log('data', test)
+                cities += test.city + " ";
+                temperatures += test.temp + "º ";
+            });
 
-        //Chamada a API axios
-        function apiCall(city){
-        axios.get('https://api.weatherbit.io/v2.0/current?city='+city+'&key=6accba4c460e4434ba36f3d1a5ac1031')
-            .then(response => {
-                const data = response.data.data;
-                // res.writeHead(200, { 'Content-Type': 'application/json' });
-                
-                
-                dataResult = {
-                    city: data[0].city_name,
-                    sunrise: data[0].sunrise,
-                    sunset: data[0].sunset,
-                    temp: data[0].temp,
-                }
-                console.log('return', JSON.stringify(dataResult))
-                return JSON.stringify(dataResult);
-
-            })
-            .catch(error => {
-                console.log("Error:" + error);
+            fs.appendFile('logs.txt', 
+            new Date() + " - " + os.platform + " Searched for:" + cities + "and got temperatures:"+ temperatures + "\n", 
+            (err) => {
+                if (err) throw "Erro aqui: " + err;
+                console.log("Error while creating log");
             });
         }
-
-        // function dummy(city) {
-        //     dataResult = {
-        //         city: city,
-        //         sunrise: "08:30",
-        //         sunset: "18:30",
-        //         temp: "13",
-        //     }
-        //     return dataResult;
-        // }
-
-        // //Ficheiro de logs(momento, plataforma e cidades)
-        // function createLog(data){
-        //     let cities = " ";
-        //     let temperatures = " ";
-        //     // console.log('data', data)
-        //     data.forEach(test => {
-        //         // console.log('data', test)
-        //         cities += test.city + " ";
-        //         temperatures += test.temp + "º ";
-        //     });
-
-        //     fs.appendFile('logs.txt', 
-        //     new Date() + " - " + os.platform + " Searched for:" + cities + "and got temperatures:"+ temperatures + "\n", 
-        //     (err) => {
-        //         if (err) throw "Erro aqui: " + err;
-        //         console.log("Error while creating log");
-        //     });
-        // }
         
     }
 });
